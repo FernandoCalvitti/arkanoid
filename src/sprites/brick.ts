68f5adac47b51ea15572e0f6dc27ad48ce7c30e6
FernandoCalvitti <83254261+FernandoCalvitti@users.noreply.github.com>
2024-01-27 18:42:12 -0300

    feat: add property, getters and setters on brick class (#5)

--- src/sprites/brick.ts
+++ src/sprites/brick.ts
@@ -1,3 +1,46 @@
 import { Vector } from "../types";
 
-export class Brick {}
+export class Brick {
+  private brickImage: HTMLImageElement = new Image();
+
+  constructor(
+    private brickWidht: number,
+    private brickHeight: number,
+    private position: Vector,
+    private brickEnergy: number,
+    image: string
+  ) {
+    this.brickWidht = brickWidht;
+    this.brickHeight = brickHeight;
+    this.position = position;
+    this.brickEnergy = brickEnergy;
+    this.brickImage.src = image;
+  }
+
+  //Getters
+  get widht(): number {
+    return this.brickWidht;
+  }
+
+  get height(): number {
+    return this.brickHeight;
+  }
+
+  get pos(): Vector {
+    return this.position;
+  }
+
+  get image(): HTMLImageElement {
+    return this.image;
+  }
+
+  get energy(): number {
+    return this.brickEnergy;
+  }
+
+  //Setters
+
+  set energy(energy: number) {
+    this.brickEnergy = energy;
+  }
+}
